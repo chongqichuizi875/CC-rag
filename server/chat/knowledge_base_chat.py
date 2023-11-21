6a1c4fb48e370da89bf2b4c7b97882fdc5753d81
liubin <lbeing_dev@qq.com>
2023-11-21 18:36:35 +0800

    llm异常输出堆栈

--- server/chat/knowledge_base_chat.py
+++ server/chat/knowledge_base_chat.py
@@ -45,6 +45,9 @@ async def knowledge_base_chat(query: str = Body(..., description="用户输入",
 
     history = [History.from_data(h) for h in history]
     
+    print("len(history)",len(history))
+    history = history[-6:]
+    
     
     #用户意图
     _model = get_ChatOpenAI(
@@ -55,46 +58,46 @@ async def knowledge_base_chat(query: str = Body(..., description="用户输入",
     # intention_prompt_template = get_prompt_template("knowledge_base_chat", "intention")
     # intention_input_msg = History(role="system", content=intention_prompt_template).to_msg_template(False)
     # intention_chat_prompt = ChatPromptTemplate.from_messages([i.to_msg_template() for i in []] + [intention_input_msg])
-    # 你是军事装备维修领域专家。
-    intention_chat_prompt =PromptTemplate(
-        template="请根据用户输入判断是否是咨询军事装备的专业问题，如果是请返回'是'，否则返回'否'。\
-        答案格式要求: '是'或者'否'。不能有其他答案 \
-        参考示例: \
-            示例1: \
-                用户输入: \
-                    '你叫什么名字' \
-                返回: \
-                    '否' \
-            示例2: \
-                用户输入: \
-                    '今天天气怎么样？'\
-                        \
-                返回: \
-                    '否' \
-            示例3: \
-                用户输入: \
-                    '能介绍一下杭州这个城市？'\
-                返回: \
-                    '否' \
-            示例4: \
-                用户输入: \
-                    '车辆发动机故障怎么维修？'\
-                返回: \
-                    '是' \
-            示例5: \
-                用户输入: \
-                    '陆军装备有哪些？' \
-                返回: \
-                    '是' \
-        用户输入: \
-            '{question}' \
-        返回: \
-        ",input_variables=["question"]
-    )
-    intention_chain = LLMChain(llm=_model,prompt=intention_chat_prompt)
-    intention = intention_chain({"question":query})
-    print("xxxx","用户意图",intention)
-    chat_intention  = "否" == intention.get("text","是")
+    # # 你是军事装备维修领域专家。
+    # intention_chat_prompt =PromptTemplate(
+    #     template="请根据用户输入判断是否是咨询军事装备的专业问题，如果是请返回'是'，否则返回'否'。\
+    #     答案格式要求: '是'或者'否'。不能有其他答案 \
+    #     参考示例: \
+    #         示例1: \
+    #             用户输入: \
+    #                 '你叫什么名字' \
+    #             返回: \
+    #                 '否' \
+    #         示例2: \
+    #             用户输入: \
+    #                 '今天天气怎么样？'\
+    #                     \
+    #             返回: \
+    #                 '否' \
+    #         示例3: \
+    #             用户输入: \
+    #                 '能介绍一下杭州这个城市？'\
+    #             返回: \
+    #                 '否' \
+    #         示例4: \
+    #             用户输入: \
+    #                 '车辆发动机故障怎么维修？'\
+    #             返回: \
+    #                 '是' \
+    #         示例5: \
+    #             用户输入: \
+    #                 '陆军装备有哪些？' \
+    #             返回: \
+    #                 '是' \
+    #     用户输入: \
+    #         '{question}' \
+    #     ",input_variables=["question"]
+    # )
+    # intention_chain = LLMChain(llm=_model,prompt=intention_chat_prompt)
+    # intention = intention_chain({"question":query})
+    # print("xxxx","用户意图",intention)
+    # chat_intention  = "否" == intention.get("text","是")
+    chat_intention = False
     
     
 
@@ -115,14 +118,20 @@ async def knowledge_base_chat(query: str = Body(..., description="用户输入",
             docs = []
             context =""
         else:
+            import time
+            t1 = time.time()*1000
+            score_threshold = 0.5
             docs = search_docs(query, knowledge_base_name, top_k, score_threshold)
+            t2 = time.time()*1000
+            t3 = t2
+            t4 = t2
             context = "\n".join([doc.page_content for doc in docs])
         
         if len(docs) == 0: ## 如果没有找到相关文档，使用Empty模板
             prompt_template = get_prompt_template("knowledge_base_chat", "Empty")
         else:
             prompt_template = get_prompt_template("knowledge_base_chat", prompt_name)
-        input_msg = History(role="user", content=prompt_template).to_msg_template(False)
+        input_msg = History(role="system", content=prompt_template).to_msg_template(False)
         chat_prompt = ChatPromptTemplate.from_messages(
             [i.to_msg_template() for i in history] + [input_msg])
 
@@ -173,19 +182,32 @@ async def knowledge_base_chat(query: str = Body(..., description="用户输入",
             base_url = request.base_url
             url = f"{base_url}knowledge_base/download_doc?" + parameters
             text = f"""出处 [{inum + 1}] [{filename}]({url}) \n\n{doc.page_content}\n\n{doc.score}\n\n"""
-
             source_documents.append(text)
-            ref_documents.append({"filename":str(os.path.basename(doc.metadata["source"])),"url":str(url),"score":doc.score,"content_pos_1":{"page_no":1,"x":0,"y":0.5},"content_pos_2":{"page_no":1,"x":1,"y":0.8}})
+            
+            starstat_index = doc.page_content.find("**")
+            if starstat_index !=-1:
+                page_content = doc.page_content[starstat_index+2:]
+            else:
+                page_content = doc.page_content
+            
+            ref_doc = {"filename":str(os.path.basename(doc.metadata["source"])),"url":str(url),"score":doc.score,"page_no":3,"page_content":page_content,"content_pos_1":{"page_no":1,"x":0,"y":0.5},"content_pos_1":{"page_no":2,"x":1,"y":0.8}}
+            print(ref_doc)
+            ref_documents.append(ref_doc)
 
         if len(source_documents) == 0: # 没有找到相关文档
             source_documents.append(f"""<span style='color:red'>未找到相关文档,该回答为大模型自身能力解答！</span>""")
 
+        token_index =0
         if stream:
             async for token in callback.aiter():
                 # Use server-sent-events to stream the response
+                token_index +=1
+                if token_index ==1:
+                    t3 = time.time()*1000
                 yield json.dumps({"answer": token}, ensure_ascii=False)
             # yield json.dumps({"docs": source_documents}, ensure_ascii=False)
-
+            t4 = time.time()*1000
+            print(">>>",str(t2-t1),str(t3-t2),str(t4-t3))
             s = json.dumps({"ref_docs": ref_documents}, ensure_ascii=False)
             yield s
             
